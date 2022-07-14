aecaa53b2147e738cbd723b016224010e01c5e38
leeuw <mohamedessabir20@gmail.com>
2022-07-14 23:21:01 +0100

    fixing 2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const port = process.env.PORT || 5000;
 
-if(process.env.MODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static('build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
